f828897175afe7fffe3f9bb5688d0c4e1208d1c2
chmoonen8 <yuechen0818@gmail.com>
2024-07-10 20:03:20 -0500

    Modify

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://chmoonen8.github.io/notesai/",
-  base: "/notesai/",
+  site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
